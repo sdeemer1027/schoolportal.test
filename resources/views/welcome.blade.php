39b686042d8f97364a79ca85dfb82daf09cfd84d
Steve Deemer <sdeemer@yahoo.com>
2023-09-23 17:14:55 -0400

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,10 +17,17 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+
+                background-image: url("/img/Hibbing_High_School_2014.jpg");
+                background-color: #cccccc;
             }
+
+
+
+
         </style>
-    </head>
-    <body class="antialiased">
+    </head><!--   Hibbing_High_School_2014.jpg    -->
+    <body class="antialiased" >
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
