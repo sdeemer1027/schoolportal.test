1d6061317498249b932009a6b179748ac03e0ac4
Steve Deemer <sdeemer@yahoo.com>
2023-09-23 11:01:50 -0400

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+
+        // Create roles
+        Role::create(['name' => 'admin']); //Main Admin
+        Role::create(['name' => 'employee']); //Admin Employee
+        Role::create(['name' => 'teacher']);  //Teachers
+        Role::create(['name' => 'coach']);    //Coaches
+        Role::create(['name' => 'counselor']); // Guidance  Counselors
+        Role::create(['name' => 'student']);   // Students
+        Role::create(['name' => 'parent']);    // Parents
+
+
+        // Create permissions
+        Permission::create(['name' => 'create articles']);
+        Permission::create(['name' => 'edit articles']);
+
+       // Assign permissions to roles
+        $adminRole = Role::findByName('admin');
+        $adminRole->givePermissionTo('create articles', 'edit articles');
+
+        $editorRole = Role::findByName('employee');
+        $editorRole->givePermissionTo('edit articles');
+
+
+        $user = User::create([
+            'name'     => 'Dr.Steve',
+            'fname'    =>'Steve',
+            'lname'    =>'Deemer',
+            'phone'    =>'954-391-0398',
+            'city'     => 'Hollywood',
+            'state'    => 'FL',
+            'zip'      =>'33020',
+            'email'    => 'dr.steve@steven.news',
+            'password' => bcrypt('password'),
+        ]);
+        $user->assignRole('admin');
+
+
+
+
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+Route::middleware(['role:admin'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['role:employee'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['role:teacher'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['role:coach'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['role:counselor'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['role:student'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['role:parent'])->group(function () {
+    // Routes accessible only by admins
+});
+
+Route::middleware(['permission:create articles'])->group(function () {
+    // Routes accessible only by users with the 'create articles' permission
+});
+
+
+
+
+
+
+
+
+
+require __DIR__.'/auth.php';
